978433e4e0aaf34647c709ca874608d23b6e35d3
akeegan97 <andrewkeegan97@gmail.com>
2023-10-28 19:11:14 -0400

    getting position correct w/sizers

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
 #include <wx/wx.h>
 #include "portfolio.hpp"
+#include "mainframe.hpp"
 
 
 class RugenBerg : public wxApp{
     public:
-        //bool OnInit()override;
+        bool OnInit()override;
         int OnExit()override;
     private:
         Portfolio portfolio;
 };
 
-// bool RugenBerg::OnInit(){
-//     portfolio.LoadFromFile("..\\storage\\data.json");
-//     return true;
+bool RugenBerg::OnInit(){
+    //portfolio.LoadFromFile("..\\storage\\data.json");
+    MainFrame *frame = new MainFrame("rugenberg", wxDefaultPosition, wxDefaultSize,portfolio);
+    // frame->SetBackgroundColour(wxColor(35, 207, 61));
+    frame->Show(true);
+    return true;
 
-// }
+}
 
 int RugenBerg::OnExit(){
-    portfolio.SavePortfolioToFile(portfolio,"..\\storage\\data.json");
+    //portfolio.SavePortfolioToFile(portfolio,"..\\storage\\data.json");
     return 0;
 }
 
-// wxIMPLEMENT_APP(RugenBerg);
+wxIMPLEMENT_APP(RugenBerg);
 
 
-int main(){
+// int main(){
 
-    std::cout<<"Ran EXE"<<std::endl;
-    return 0;
-}
+//     std::cout<<"Ran EXE"<<std::endl;
+//     return 0;
+// }
--- src/mainframe.cpp
+++ src/mainframe.cpp
@@ -2,11 +2,56 @@
 
 
 void MainFrame::setupLayout(){
-   wxBoxSizer* hBox = new wxBoxSizer(wxHORIZONTAL);
-   wxBoxSizer* topThird = new wxBoxSizer(wxVERTICAL);
 
-   // assetListControl = new VListControl<Asset>(this, wxID_ANY,wxDefaultPosition, wxDefaultSize);
-   // investorPositionListControl = new VListControl<InvestorPositionDisplay>(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
+   auto mainSizer = new wxBoxSizer(wxVERTICAL);
+   wxBoxSizer* topSizer = new wxBoxSizer(wxHORIZONTAL);
+   wxPanel* a1Panel = new wxPanel(this);
+   topSizer->Add(a1Panel, wxEXPAND | wxALL, 10);
+   mainSizer->Add(topSizer, 1, wxEXPAND | wxALL, 10); // 10 units border
+
+   // Middle section (80%)
+   wxBoxSizer* middleSizer = new wxBoxSizer(wxHORIZONTAL);
+   mainSizer->Add(middleSizer, 8, wxEXPAND | wxALL, 10);
+
+   // LSide (60% of middle section)
+   wxBoxSizer* lSideSizer = new wxBoxSizer(wxVERTICAL);
+   middleSizer->Add(lSideSizer, 5, wxEXPAND | wxALL, 10);
+   // LSide - Top (30%)
+   lSideSizer->Add(new wxPanel(this), 3, wxEXPAND | wxALL, 10);
+   // LSide - Bottom (70%)
+   lSideSizer->Add(new wxPanel(this), 7, wxEXPAND | wxALL, 10);
+
+   // RSide (40% of middle section)
+   wxBoxSizer* rSideSizer = new wxBoxSizer(wxVERTICAL);
+   middleSizer->Add(rSideSizer, 5, wxEXPAND | wxALL, 10);
+   // RSide - Top (20%)
+   rSideSizer->Add(new wxPanel(this), 2, wxEXPAND | wxALL, 10);
+   // RSide - Middle (50%)
+   rSideSizer->Add(new wxPanel(this), 5, wxEXPAND | wxALL, 10);
+   // RSide - Bottom (30%)
+   rSideSizer->Add(new wxPanel(this), 3, wxEXPAND | wxALL, 10);
+
+   // Bottom horizontal section (10%)
+   wxBoxSizer* bottomSizer = new wxBoxSizer(wxHORIZONTAL);
+   mainSizer->Add(bottomSizer, 1, wxEXPAND | wxALL, 10);
+
+   this->SetSizer(mainSizer);
    
-    
 }
+//Desired Layout of the mainpage of application
+// +---------------------------------------+
+// |                  A1                   | <- 10%
+// +---------------------------------------+
+// |    LSide         |       RSide        |
+// |                  |                    |
+// |                  |       TSection     |
+// |                  |         20%        |
+// |     TSection     +--------------------+
+// |       30%        |       MSection     |
+// |                  |         50%        |
+// |                  +--------------------+
+// |     BSection     |       BSection     |
+// |       70%        |         30%        |
+// +---------------------------------------+
+// |                  A3                   | <- 10%
+// +---------------------------------------+
