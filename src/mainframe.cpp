718926421e0c6c5c0a682d180cb33655a506943e
akeegan97 <andrewkeegan97@gmail.com>
2023-10-29 02:25:51 -0400

    dialing in layout of the main frame of application

--- src/mainframe.cpp
+++ src/mainframe.cpp
@@ -4,36 +4,50 @@
 void MainFrame::setupLayout(){
 
    auto mainSizer = new wxBoxSizer(wxVERTICAL);
-   wxBoxSizer* topSizer = new wxBoxSizer(wxHORIZONTAL);
+   
    wxPanel* a1Panel = new wxPanel(this);
-   topSizer->Add(a1Panel, wxEXPAND | wxALL, 10);
-   mainSizer->Add(topSizer, 1, wxEXPAND | wxALL, 10); // 10 units border
+   a1Panel->SetBackgroundColour(wxColour(200, 200, 200));  
+   mainSizer->Add(a1Panel, 1, wxEXPAND | wxALL, 20);
 
-   // Middle section (80%)
+ 
    wxBoxSizer* middleSizer = new wxBoxSizer(wxHORIZONTAL);
    mainSizer->Add(middleSizer, 8, wxEXPAND | wxALL, 10);
 
-   // LSide (60% of middle section)
+   
    wxBoxSizer* lSideSizer = new wxBoxSizer(wxVERTICAL);
-   middleSizer->Add(lSideSizer, 5, wxEXPAND | wxALL, 10);
-   // LSide - Top (30%)
-   lSideSizer->Add(new wxPanel(this), 3, wxEXPAND | wxALL, 10);
-   // LSide - Bottom (70%)
-   lSideSizer->Add(new wxPanel(this), 7, wxEXPAND | wxALL, 10);
 
-   // RSide (40% of middle section)
+   middleSizer->Add(lSideSizer, 5, wxEXPAND | wxALL, 10);
+   wxPanel* topLSidePanel = new wxPanel(this);
+   topLSidePanel->SetBackgroundColour(wxColor(35, 207, 61));
+   lSideSizer->Add(topLSidePanel, 3, wxEXPAND | wxALL, 10);
+  
+   wxPanel*bottomLSidePanel = new wxPanel(this);
+   bottomLSidePanel->SetBackgroundColour(wxColor(207, 35, 141));
+   lSideSizer->Add(bottomLSidePanel, 7, wxEXPAND | wxALL, 10);
+
+  
    wxBoxSizer* rSideSizer = new wxBoxSizer(wxVERTICAL);
    middleSizer->Add(rSideSizer, 5, wxEXPAND | wxALL, 10);
-   // RSide - Top (20%)
-   rSideSizer->Add(new wxPanel(this), 2, wxEXPAND | wxALL, 10);
-   // RSide - Middle (50%)
-   rSideSizer->Add(new wxPanel(this), 5, wxEXPAND | wxALL, 10);
-   // RSide - Bottom (30%)
-   rSideSizer->Add(new wxPanel(this), 3, wxEXPAND | wxALL, 10);
+  
+   wxPanel* topRSidePanel = new wxPanel(this);
+   topRSidePanel->SetBackgroundColour(wxColor(178, 35, 207));
+   rSideSizer->Add(topRSidePanel, 2, wxEXPAND | wxALL, 10);
+   
+   wxPanel* midRSidePanel = new wxPanel(this);
+   midRSidePanel->SetBackgroundColour(wxColor(242, 153, 212));
+   rSideSizer->Add(midRSidePanel, 5, wxEXPAND | wxALL, 10);
+   
+   wxPanel* botRSidePanel = new wxPanel(this);
+   botRSidePanel->SetBackgroundColour(wxColor(82, 227, 159));
+   rSideSizer->Add(botRSidePanel, 3, wxEXPAND | wxALL, 10);
 
-   // Bottom horizontal section (10%)
-   wxBoxSizer* bottomSizer = new wxBoxSizer(wxHORIZONTAL);
-   mainSizer->Add(bottomSizer, 1, wxEXPAND | wxALL, 10);
+   
+   wxPanel* a3Panel = new wxPanel(this);
+   a3Panel->SetBackgroundColour(wxColour(200, 200, 200));  
+   auto bottomSizer = new wxBoxSizer(wxVERTICAL);
+   bottomSizer->Add(a3Panel, 1, wxEXPAND | wxALL, 20);
+
+   mainSizer->Add(bottomSizer, 1, wxEXPAND | wxALL, 10);  
 
    this->SetSizer(mainSizer);
    
