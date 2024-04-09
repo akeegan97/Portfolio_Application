fc01402ae870747d7d4275604708863bd676ac28
akeegan97 <andrewkeegan97@gmail.com>
2024-04-09 08:40:57 -0400

    cleaning up json files for clarity and testing purposes

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ bool RugenBerg::OnInit()
     wxString resourcePath = wxStandardPaths::Get().GetResourcesDir();
     jsonFilePath = resourcePath + "/data/Fresh.json";
     #elif defined(__WXMSW__)
-    jsonFilePath = "../resources/data/KAWACRETEST.json";
+    jsonFilePath = "../resources/data/CRECPKAWABASE.json";
     #endif
 
     portfolio.LoadFromFile(jsonFilePath);
@@ -39,7 +39,7 @@ int RugenBerg::OnExit()
     jsonFilePath = resourcePath + "/data/Fresh.json";
     #endif
     #ifdef __WXMSW__
-    jsonFilePath = "../resources/data/KAWACRETESTOUTPUT.json";
+    jsonFilePath = "../resources/data/DISTRIBUTIONOUTPUT.json";
     #endif
     portfolio.SavePortfolioToFile(portfolio, jsonFilePath);
     return wxApp::OnExit();
